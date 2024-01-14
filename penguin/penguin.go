3029bfa998947dc340a9c65955c8abf5076c0bd1
Maxxwell Gentry <maxx@ip-172-31-4-12.us-east-2.compute.internal>
2024-01-14 21:03:24 +0000

    Restructure

--- penguin/penguin.go
+++ penguin/penguin.go
@@ -1,1 +1,29 @@
-package penguin
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func Check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+
+	if os.Args[1] == "" {
+		return
+	}
+
+	file := os.Args[1]
+	dat, err := os.ReadFile(file)
+	Check(err)
+
+	for _, line := range dat {
+		fmt.Println(line)
+	}
+
+	return
+}
