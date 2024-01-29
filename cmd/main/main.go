54bc0aa20ca3f042764a26ec811c7ea921441957
Maxxwell Gentry <maxx@ip-172-31-4-12.us-east-2.compute.internal>
2024-01-29 07:56:44 +0000

    Assembly generator working for current grammar, automated assembly/linking, and added comment functionality to language

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
 
+	"github.com/GenM4/penguin/pkg/generator"
 	"github.com/GenM4/penguin/pkg/parser"
 	"github.com/GenM4/penguin/pkg/tokenizer"
 )
@@ -54,5 +58,52 @@ func main() {
 		}
 	}
 
+	srcFilepath := os.Args[len(os.Args)-1]
+	srcFilename := filepath.Base(srcFilepath)
+	baseFilepath := filepath.Dir(srcFilepath)
+	baseFilename := removeFileExtension(srcFilename)
+	asmFilename := baseFilename + ".asm"
+	asmFilepath := filepath.Join(baseFilepath, asmFilename)
+	objFilename := baseFilename + ".o"
+	objFilepath := filepath.Join(baseFilepath, objFilename)
+	execFilepath := filepath.Join(baseFilepath, baseFilename)
+
+	asmFile, err := os.OpenFile(asmFilepath, os.O_CREATE|os.O_RDWR, 0755)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Opened file: " + asmFilepath)
+
+	if err = os.Truncate(asmFilepath, 0); err != nil {
+		panic(err)
+	}
+
+	generator.Generate(ASTRoot, asmFile)
+
+	log.Println("Completed generating assembly to " + asmFilepath)
+
+	assembleCmd := exec.Command("nasm", "-felf64", asmFilename)
+	assembleCmd.Dir = baseFilepath
+	if err = assembleCmd.Run(); err != nil {
+		log.Println("Assembly failed")
+		panic(err)
+	}
+
+	log.Println("Completed assembly to " + objFilepath)
+
+	linkCmd := exec.Command("ld", objFilename, "-o", baseFilename)
+	linkCmd.Dir = baseFilepath
+	if err = linkCmd.Run(); err != nil {
+		log.Print(err)
+		panic(err)
+	}
+
+	log.Println("Completed linking to " + execFilepath)
+
 	return
+}
+
+func removeFileExtension(filename string) string {
+	return filename[:len(filename)-len(filepath.Ext(filename))]
 }
