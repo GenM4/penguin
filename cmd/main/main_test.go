3afe4486747c6a2d2c4ec99593b25ad525cf8fb7
Maxxwell Gentry <maxx@ip-172-31-4-12.us-east-2.compute.internal>
2024-01-29 17:31:02 +0000

    Added testing for compiler, limited functionality for now as exit() is the only statement

---
+++ cmd/main/main_test.go
@@ -0,0 +1,32 @@
+package main_test
+
+import (
+	"flag"
+	"os/exec"
+	"testing"
+)
+
+var expected = flag.String("expect", "0", "Expected exit code for compiled executable")
+
+func TestCompile(t *testing.T) {
+
+	defer ExecuteProgram(t)
+	CompileProgram(t)
+}
+
+func CompileProgram(t *testing.T) {
+	runCompile := exec.Command("go", "run", ".", "../../test/testfile.pn")
+	if err := runCompile.Run(); err != nil {
+		t.Errorf("Compilation did not complete, error: %v", err)
+	}
+}
+
+func ExecuteProgram(t *testing.T) {
+	runExec := exec.Command("./testfile")
+	runExec.Dir = "../../test"
+	_, err := runExec.Output()
+	if err != nil {
+		t.Errorf("Program exited with %v, expected %v", err, *expected)
+	}
+
+}
